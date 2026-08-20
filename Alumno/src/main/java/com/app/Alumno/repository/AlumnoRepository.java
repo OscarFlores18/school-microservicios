149a0947317abb3d6b8eac371a8f100d053f291c
Oscar Flores <48716927@fatimarem.edu.ar>
2026-08-19 21:18:04 -0300

    Testeo y implementacion de swagger en alumnos, config server hecho.

---
+++ Alumno/src/main/java/com/app/Alumno/repository/AlumnoRepository.java
@@ -0,0 +1,18 @@
+package com.app.Alumno.repository;
+
+import com.app.Alumno.domain.Alumno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AlumnoRepository extends JpaRepository<Alumno, Long> {
+
+    Optional<Alumno> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByDni(String dni);
+
+    List<Alumno> findByCursoIgnoreCase(String curso);
+}
